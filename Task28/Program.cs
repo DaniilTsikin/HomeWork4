971f2c6092055e6040be2da29d342bdeacac6248
Daniil <dtsikin@mail.ru>
2022-02-19 19:32:11 +0500

    Complete Task28

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Подсчитать сумму цифр в числе
+
+//Получаем число
+
+int GetValueInt(string message)
+{
+    Console.Write(message);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+//Создаём метод, для вычисления суммы цифр в полученном числе
+
+int SumDigitNumber(int num)
+{
+   int sumDigit = 0;
+   while(num > 0)
+   {
+    sumDigit = sumDigit + (num % 10);
+    num = num / 10;
+   }
+   return sumDigit;
+}
+
+int number = GetValueInt("Введите число: ");
+Console.WriteLine($"Дано число {number}");
+
+int resultSum = SumDigitNumber(number);
+Console.WriteLine($"Результат {resultSum}");
+
