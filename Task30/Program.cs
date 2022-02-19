f47c775827c93b7f2005177e4c1d5d5591de5785
Daniil <dtsikin@mail.ru>
2022-02-19 21:03:58 +0500

    Change Task30

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -1,11 +1,13 @@
 ﻿//Показать кубы чисел, заканчивающихся на четную цифру
 
+//Создаем функцию для нахождение кубов чисел до number
 void GetCubeNumberS(int number)
 {
     int result = 0;
     for (int n = 1; n <= number; n++)
     {
         result = n*n*n;
+        //Исключаем нечётные кубы
         if(result % 2 == 0)
         {
             Console.Write($"{result} ");
