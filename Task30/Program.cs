3775954253141927875a14dac52a3ce31ce03d8c
Daniil <dtsikin@mail.ru>
2022-02-19 20:15:02 +0500

    Complete Task30

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿//Показать кубы чисел, заканчивающихся на четную цифру
+
+void GetCubeNumberS(int number)
+{
+    int result = 0;
+    for (int n = 1; n <= number; n++)
+    {
+        result = n*n*n;
+        if(result % 2 == 0)
+        {
+            Console.Write($"{result} ");
+        }
+        else
+        {
+            Console.Write("");
+        }
+    }
+}
+
+Console.Write("Введите любое число от 1 и больше: ");
+int input = int.Parse(Console.ReadLine());
+
+GetCubeNumberS(input);
