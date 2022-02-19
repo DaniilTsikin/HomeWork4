844c4099316bf5cf5451db761bb3b6790d1105aa
Daniil <dtsikin@mail.ru>
2022-02-19 21:02:33 +0500

    Complete Task32

--- Task32/Program.cs
+++ Task32/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+//Получаем число
+int GetValueIntFromConsole(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    return int.Parse(value);
+}
+
+//Создаем метод для инициализации массива от 0 до 1
+int[] ArrayInit(int number)
+{
+    int[] array = new int[number];
+    Random randomValue = new Random();
+
+    for(int i = 0; i < number; i++)
+    {
+        array[i] = randomValue.Next(0, 2);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach(int item in array)
+    {
+        Console.Write($"{array[item]}");
+    }
+}
+
+//Создаем переменную с помощью которой вызываем метод для получения числа 
+int getInt = GetValueIntFromConsole("Введите число элементов массива: ");
+//Создаем переменную с помощью которой вызываем метод для инициализации массива
+int[] array = ArrayInit(getInt);
+
+PrintArray(array);
