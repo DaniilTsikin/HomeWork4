4f0ac747e8551e2f82045d63b633bc50f64f6c7c
Daniil <dtsikin@mail.ru>
2022-02-19 18:18:22 +0500

    complete Task26

--- Task26/Program.cs
+++ Task26/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Возведите число А в натуральную степень B используя цикл
+
+//Получаем число
+int GetValueInt(string message)
+{
+    Console.Write(message);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+//Получаем степень
+int GetPower(string message)
+{
+     Console.Write(message);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+//Возводим полученное число в полученную степень
+int RaisingANumberToAPower(int num1, int num2)
+{   
+    int result = 1;
+    for(int i = 0; i < num2; i++)
+    {
+        result = result*num1;
+    }
+    return result;
+}
+
+int valueInt = GetValueInt("Введите число: ");
+Console.WriteLine($"Число: {valueInt} ");
+
+int power = GetPower("Введите степень числа: ");
+Console.WriteLine($"Степень: {power}");
+
+int raisingNumber = RaisingANumberToAPower(valueInt, power);
+Console.WriteLine($"Резултат возведения: {raisingNumber}");
